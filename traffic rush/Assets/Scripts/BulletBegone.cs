da03b19f32ca1daf3893cdb41de81f0f4222a7b0
Rory <rwilson2@ltu.edu>
2021-02-16 00:20:16 -0500

    rooty tooty aim and shooty
    - you can now Fire the Gun

---
+++ traffic rush/Assets/Scripts/BulletBegone.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBegone : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
--- traffic rush/Assets/Scripts/PlayerTurretScript.cs
+++ traffic rush/Assets/Scripts/PlayerTurretScript.cs
@@ -6,11 +6,24 @@ public class PlayerTurretScript : MonoBehaviour
 {
     private Camera cam;
     private Vector2 thisPos, mousePos, diffPos;
+    public GameObject tip, bullet;
+    public float speed;
 
     // Start is called before the first frame update
     void Start()
     {
         cam = Camera.main;
+    }
+
+    void Shoot()
+    {
+        var count = GameObject.FindGameObjectsWithTag("PlayerBullet");
+
+        if (count.Length < 3)
+        {
+            GameObject clone = Instantiate(bullet, tip.transform.position, tip.transform.rotation);
+            clone.GetComponent<Rigidbody2D>().velocity = tip.transform.up * speed;
+        }
     }
 
     // Update is called once per frame
@@ -20,5 +33,10 @@ public class PlayerTurretScript : MonoBehaviour
 
         var angle = Mathf.Atan2(diffPos.y, diffPos.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
     }
 }
